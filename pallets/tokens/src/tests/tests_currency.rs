d53d0fd0e3f486b0792af8e1dccad30ca8739a7c
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-10-14 17:08:50 +0200

    move tests to their own subfolder as we expect this to grow

--- pallets/tokens/src/tests/tests_currency.rs
+++ pallets/tokens/src/tests/tests_currency.rs
@@ -21,9 +21,9 @@ use frame_support::{assert_noop, assert_ok};
 fn it_works_for_default_value() {
     new_test_ext().execute_with(|| {
         // Dispatch a signed extrinsic.
-        assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
+        assert_ok!(TestModule::do_something(Origin::signed(1), 42));
         // Read pallet storage and assert an expected result.
-        assert_eq!(TemplateModule::something(), Some(42));
+        assert_eq!(TestModule::something(), Some(42));
     });
 }
 
@@ -32,7 +32,7 @@ fn correct_error_for_none_value() {
     new_test_ext().execute_with(|| {
         // Ensure the expected error is thrown when no value is present.
         assert_noop!(
-            TemplateModule::cause_error(Origin::signed(1)),
+            TestModule::cause_error(Origin::signed(1)),
             Error::<Test>::NoneValue
         );
     });
