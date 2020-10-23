0200f816975e738bcb7bc5b0fa67438de6b05c22
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-10-23 16:48:21 +0200

    fmt all

--- runtime/src/weights/frame_system.rs
+++ runtime/src/weights/frame_system.rs
@@ -20,37 +20,37 @@ use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 pub struct WeightInfo;
 impl frame_system::WeightInfo for WeightInfo {
-	// WARNING! Some components were not used: ["b"]
-	fn remark() -> Weight {
-		2_106_000 as Weight
-	}
-	fn set_heap_pages() -> Weight {
-		(5_000_000 as Weight).saturating_add(DbWeight::get().writes(1 as Weight))
-	}
-	// WARNING! Some components were not used: ["d"]
-	fn set_changes_trie_config() -> Weight {
-		(14_307_000 as Weight)
-			.saturating_add(DbWeight::get().reads(1 as Weight))
-			.saturating_add(DbWeight::get().writes(2 as Weight))
-	}
-	fn set_storage(i: u32) -> Weight {
-		(0 as Weight)
-			.saturating_add((1_599_000 as Weight).saturating_mul(i as Weight))
-			.saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(i as Weight)))
-	}
-	fn kill_storage(i: u32) -> Weight {
-		(4_553_000 as Weight)
-			.saturating_add((1_004_000 as Weight).saturating_mul(i as Weight))
-			.saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(i as Weight)))
-	}
-	fn kill_prefix(p: u32) -> Weight {
-		(10_358_000 as Weight)
-			.saturating_add((1_938_000 as Weight).saturating_mul(p as Weight))
-			.saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(p as Weight)))
-	}
-	fn suicide() -> Weight {
-		(71_000_000 as Weight)
-			.saturating_add(DbWeight::get().reads(4 as Weight))
-			.saturating_add(DbWeight::get().writes(2 as Weight))
-	}
+    // WARNING! Some components were not used: ["b"]
+    fn remark() -> Weight {
+        2_106_000 as Weight
+    }
+    fn set_heap_pages() -> Weight {
+        (5_000_000 as Weight).saturating_add(DbWeight::get().writes(1 as Weight))
+    }
+    // WARNING! Some components were not used: ["d"]
+    fn set_changes_trie_config() -> Weight {
+        (14_307_000 as Weight)
+            .saturating_add(DbWeight::get().reads(1 as Weight))
+            .saturating_add(DbWeight::get().writes(2 as Weight))
+    }
+    fn set_storage(i: u32) -> Weight {
+        (0 as Weight)
+            .saturating_add((1_599_000 as Weight).saturating_mul(i as Weight))
+            .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(i as Weight)))
+    }
+    fn kill_storage(i: u32) -> Weight {
+        (4_553_000 as Weight)
+            .saturating_add((1_004_000 as Weight).saturating_mul(i as Weight))
+            .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(i as Weight)))
+    }
+    fn kill_prefix(p: u32) -> Weight {
+        (10_358_000 as Weight)
+            .saturating_add((1_938_000 as Weight).saturating_mul(p as Weight))
+            .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(p as Weight)))
+    }
+    fn suicide() -> Weight {
+        (71_000_000 as Weight)
+            .saturating_add(DbWeight::get().reads(4 as Weight))
+            .saturating_add(DbWeight::get().writes(2 as Weight))
+    }
 }
--- runtime/src/weights/pallet_timestamp.rs
+++ runtime/src/weights/pallet_timestamp.rs
@@ -3,18 +3,18 @@
 #![allow(unused_parens)]
 #![allow(unused_imports)]
 
-use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
+use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 pub struct WeightInfo;
 impl pallet_timestamp::WeightInfo for WeightInfo {
-	// WARNING! Some components were not used: ["t"]
-	fn set() -> Weight {
-		(32_292_000 as Weight)
-			.saturating_add(DbWeight::get().reads(3 as Weight))
-			.saturating_add(DbWeight::get().writes(2 as Weight))
-	}
-	// WARNING! Some components were not used: ["t"]
-	fn on_finalize() -> Weight {
-		(9_632_000 as Weight)
-	}
+    // WARNING! Some components were not used: ["t"]
+    fn set() -> Weight {
+        (32_292_000 as Weight)
+            .saturating_add(DbWeight::get().reads(3 as Weight))
+            .saturating_add(DbWeight::get().writes(2 as Weight))
+    }
+    // WARNING! Some components were not used: ["t"]
+    fn on_finalize() -> Weight {
+        (9_632_000 as Weight)
+    }
 }
