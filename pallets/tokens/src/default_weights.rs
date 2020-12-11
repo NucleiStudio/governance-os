514912aa922ff7268d1b8342c0ce10729f24f6d9
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-12-11 15:48:55 +0100

    rebench on apple silicon

--- pallets/tokens/src/default_weights.rs
+++ pallets/tokens/src/default_weights.rs
@@ -16,31 +16,35 @@
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
 
+#![allow(unused_parens)]
+#![allow(unused_imports)]
+
 use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
-impl crate::WeightInfo for () {
+pub struct WeightInfo;
+impl governance_os_pallet_tokens::WeightInfo for WeightInfo {
     fn create() -> Weight {
-        (123_000_000 as Weight)
+        (114_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(7 as Weight))
             .saturating_add(DbWeight::get().writes(5 as Weight))
     }
     fn mint() -> Weight {
-        (118_000_000 as Weight)
+        (105_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(8 as Weight))
             .saturating_add(DbWeight::get().writes(4 as Weight))
     }
     fn burn() -> Weight {
-        (118_000_000 as Weight)
+        (105_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(8 as Weight))
             .saturating_add(DbWeight::get().writes(4 as Weight))
     }
     fn update_details() -> Weight {
-        (125_000_000 as Weight)
+        (113_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(7 as Weight))
             .saturating_add(DbWeight::get().writes(4 as Weight))
     }
     fn transfer() -> Weight {
-        (150_000_000 as Weight)
+        (135_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(7 as Weight))
             .saturating_add(DbWeight::get().writes(3 as Weight))
     }
