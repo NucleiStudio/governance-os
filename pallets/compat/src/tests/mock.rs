0205996b6ded90d7aeac9be91a2ac6efdf5bac44
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-04-07 15:18:56 +0200

    compat to sub3 + frame v2

--- pallets/compat/src/tests/mock.rs
+++ pallets/compat/src/tests/mock.rs
@@ -14,19 +14,116 @@
  * limitations under the License.
  */
 
-use crate::{Module, Trait};
-use governance_os_support::mock_runtime;
+use crate as governance_os_pallet_compat;
+use codec::{Decode, Encode};
+use frame_support::{construct_runtime, parameter_types, traits::GenesisBuild};
+use governance_os_support::{
+    impl_enum_default,
+    testing::primitives::{AccountId, BlockNumber, CurrencyId},
+};
+use serde::{Deserialize, Serialize};
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::IdentityLookup, RuntimeDebug};
 
-mock_runtime!(Test);
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
 
-impl Trait for Test {
-    type Event = ();
+construct_runtime!(
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Module, Call, Config, Storage, Event<T>},
+        Bylaws: governance_os_pallet_bylaws::{Module, Call, Storage, Event<T>},
+        Compat: governance_os_pallet_compat::{Module, Call, Event<T>},
+    }
+);
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub BlockWeights: frame_system::limits::BlockWeights =
+        frame_system::limits::BlockWeights::simple_max(1024);
+    pub static ExistentialDeposit: u64 = 0;
+}
+impl frame_system::Config for Test {
+    type BaseCallFilter = ();
+    type BlockWeights = BlockWeights;
+    type BlockLength = ();
+    type DbWeight = ();
+    type Origin = Origin;
+    type Index = u64;
+    type BlockNumber = BlockNumber;
+    type Call = Call;
+    type Hash = H256;
+    type Hashing = ::sp_runtime::traits::BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = ();
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+}
+#[derive(
+    Eq,
+    PartialEq,
+    RuntimeDebug,
+    Encode,
+    Decode,
+    Copy,
+    Clone,
+    Serialize,
+    Deserialize,
+    Ord,
+    PartialOrd,
+)]
+pub enum MockRoles {
+    Root,
+    RemarkOnly,
+    CreateCurrencies,
+    TransferCurrency(CurrencyId),
+    ManageCurrency(CurrencyId),
+    CreateOrganizations,
+    ApplyAsOrganization(AccountId),
+}
+impl_enum_default!(MockRoles, RemarkOnly);
+impl governance_os_pallet_bylaws::RoleBuilder for MockRoles {
+    type Role = MockRoles;
+
+    fn manage_roles() -> MockRoles {
+        Self::root()
+    }
+
+    fn root() -> MockRoles {
+        MockRoles::Root
+    }
+}
+
+parameter_types! {
+    pub const RootRole: MockRoles = MockRoles::Root;
+    pub const MaxRoles: u32 = 5;
+}
+
+impl governance_os_pallet_bylaws::Config for Test {
+    type Event = Event;
+    type Role = MockRoles;
+    type WeightInfo = ();
+    type MaxRoles = MaxRoles;
+    type RoleBuilder = MockRoles;
+}
+
+impl governance_os_pallet_compat::Config for Test {
+    type Event = Event;
     type Call = Call;
     type RoleManager = Bylaws;
     type RoleBuilder = MockRoles;
 }
-
-pub type Compat = Module<Test>;
 
 pub struct ExtBuilder {
     root: Option<AccountId>,
