e159843ec8d1b81f815106de8b7dde093392e865
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-06-02 11:42:10 +0200

    helpers code dedup

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -4,56 +4,58 @@ const parseCall = (api, call) => {
     return `${parsed.section}.${parsed.method}(${parsed.args})`;
 }
 
-/// return wether a proposal will pass or expire for the coin voting system
-const coinVotingState = (state, totalSupply, now) => {
-    const totalParticipation = state["total_favorable"].add(state["total_against"]);
-    const minParticipation = state.parameters["min_participation"] / 100;
-    const minQuorum = state.parameters["min_quorum"] / 100;
-    const totalFavorable = state["total_favorable"];
-    const createdOn = state["created_on"].toNumber();
-    const ttl = state.parameters.ttl.toNumber();
-
+const proposalPassing = (totalParticipation, totalFavorable, minParticipation, minQuorum, totalSupply) => {
     const enoughParticipation = totalParticipation > minParticipation * totalSupply;
     const enoughQuorum = totalFavorable > minQuorum * totalParticipation;
-    const proposalPassing = enoughParticipation && enoughQuorum;
+    return enoughParticipation && enoughQuorum;
+};
 
-    const proposalExpired = now > createdOn + ttl;
+const proposalExpired = (now, createdOn, ttl) => {
+    return now > createdOn + ttl;
+};
 
-    return [proposalPassing, proposalExpired];
+/// return wether a proposal will pass or expire for the coin voting system
+const coinVotingState = (state, totalSupply, now) => {
+    return [
+        proposalPassing(
+            state["total_favorable"].add(state["total_against"]),
+            state["total_favorable"],
+            state.parameters["min_participation"] / 100,
+            state.parameters["min_quorum"] / 100,
+            totalSupply
+        ),
+        proposalExpired(now, state["created_on"].toNumber(), state.parameters.ttl.toNumber())
+    ];
 };
 
 /// return wether a proposal will pass or expire for the conviction voting system
-const convictionVotingState = (state, totalSupply, now) => {
-    // we only support closing a conviction voting proposal once it is expired
-    // as conviction accumulates over time, it needs to be computed regularly
-    // and this would amount to lot of duplicated code here. in the future,
-    // we may add a RPC call for it.
-    const proposalPassing = false;
-
-    const createdOn = state["created_on"].toNumber();
-    const ttl = state.parameters.ttl.toNumber();
-
-    const proposalExpired = now > createdOn + ttl;
-
-    return [proposalPassing, proposalExpired];
+const convictionVotingState = (state, _totalSupply, now) => {
+    return [
+        // we only support closing a conviction voting proposal once it is expired
+        // as conviction accumulates over time, it needs to be computed regularly
+        // and this would amount to lot of duplicated code here. in the future,
+        // we may add a RPC call for it.
+        false,
+        proposalExpired(now, state["created_on"].toNumber(), state.parameters.ttl.toNumber())
+    ];
 };
 
 /// return wether a proposal will pass or expire for the plcr voting system
 const plcrVotingState = (state, totalSupply, now) => {
-    const totalParticipation = state["revealed_favorable"].add(state["revealed_against"]);
-    const minParticipation = state.parameters["min_participation"] / 100;
-    const minQuorum = state.parameters["min_quorum"] / 100;
-    const totalFavorable = state["revealed_favorable"];
-    const createdOn = state["created_on"].toNumber();
-    const ttl = state.parameters["commit_duration"].add(state.parameters["reveal_duration"]).toNumber();
-
-    const enoughParticipation = totalParticipation > minParticipation * totalSupply;
-    const enoughQuorum = totalFavorable > minQuorum * totalParticipation;
-    const proposalPassing = enoughParticipation && enoughQuorum;
-
-    const proposalExpired = now > createdOn + ttl;
-
-    return [proposalPassing, proposalExpired];
+    return [
+        proposalPassing(
+            state["revealed_favorable"].add(state["revealed_against"]),
+            state["revealed_favorable"],
+            state.parameters["min_participation"] / 100,
+            state.parameters["min_quorum"] / 100,
+            totalSupply
+        ),
+        proposalExpired(
+            now,
+            state["created_on"].toNumber(),
+            state.parameters["commit_duration"].add(state.parameters["reveal_duration"]).toNumber()
+        )
+    ];
 };
 
 export {
